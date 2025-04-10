eb2c88f0a5606d3f91379a47131fe16780484db4
almafra <almafra@miamioh.edu>
2025-04-10 17:28:30 -0400

    Update test_html_elements.py

--- test_html_elements.py
+++ test_html_elements.py
@@ -1,37 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import unittest
 
 class TestH5Tag(unittest.TestCase):
     def setUp(self):
         # Setup Firefox options
         firefox_options = Options()
-        firefox_options.add_argument("--headless")  # Run in headless mode
+        firefox_options.add_argument("--headless")  # Ensures the browser window does not open
         firefox_options.add_argument("--no-sandbox")
         firefox_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Firefox(options=firefox_options)
 
     def test_h5_tag_content(self):
         driver = self.driver
-        driver.get("http://10.48.10.147")  # Replace with your actual Cluster IP
-
-        try:
-            # Wait up to 10 seconds for the <h5> tag to appear
-            h5_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.TAG_NAME, "h5"))
-            )
-            h5_text = h5_element.text
-        except TimeoutException:
-            self.fail("Timed out waiting for <h5> tag to be present.")
-        except NoSuchElementException:
-            self.fail("No <h5> tag found on the page.")
-        else:
-            # Assert that the text of the <h5> tag is as expected
-            self.assertEqual("Lab 3-6 Works!", h5_text, "The <h5> tag does not contain the expected text.")
+        driver.get("http://10.48.10.147")  # Replace with your target website
+        
+        # Locate the <h5> tag and get its text
+        h5_text = driver.find_element(By.TAG_NAME, "h5").text
+        
+        # Assert that the text of the <h5> tag is "Lab 3-6 Works!"
+        self.assertEqual("Lab 3-6 Works!", h5_text, "The <h5> tag does not contain the text 'Lab 3-6 Works!'")
 
     def tearDown(self):
         self.driver.quit()
